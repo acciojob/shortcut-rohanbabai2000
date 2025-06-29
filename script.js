d6f2077a411b025d6b55157d07f0fbcb7a3e6419
ROHAN DAS <61402717+rohanbabai2000@users.noreply.github.com>
2025-06-29 19:34:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function shortcut(s1, s2) {
   // your code here
+	let p=s1.length;
+	let q=s2.length;
+	if((p===0)&&(q===0)){
+		return 0;
+	}
+	let c=s1[0];
+	let d=s2[0];
+	return (c+" "+d);
 }
 
 // Do not change the code below.
